746256bd393500d4fc5f9f912274c8ff50327b4b
Minerva04 <1249805139@qq.com>
2023-07-06 11:04:59 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/ztq/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztq.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
